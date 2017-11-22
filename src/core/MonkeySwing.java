2ffcd21aed70a2923138b4a7c85782ac9570a164
putta-jon <thejsen@live.dk>
2017-11-22 10:02:47 +0100

    Cleanup and better swing example

--- src/core/MonkeySwing.java
+++ src/core/MonkeySwing.java
@@ -1,66 +1,61 @@
 package core;
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Frame;
+import java.awt.FlowLayout;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;        
 
 public class MonkeySwing {
-	public static final int FIVE_SECONDS = 5000;
-	public static final int MAX_Y = 400;
-	public static final int MAX_X = 400;
-
-
 	/**
-	 * Create the GUI and show it.  For thread safety,
-	 * this method should be invoked from the
-	 * event-dispatching thread.
+	 * Creating a gui here only for proof of concept
 	 */
 	public static void createAndShowGUI() {
-		JFrame frame = new JFrame("HelloWorldSwing");
+		JFrame frame = new JFrame("MonkeySwing testing");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JTextField field = new JTextField("aa");
-		frame.getContentPane().add(field);
+		frame.setLayout(new FlowLayout());
+		JTextField field1 = new JTextField();
+		field1.setBounds(10,20,165,25);
+		frame.add(field1);
+		JTextField field2 = new JTextField();
+		field2.setBounds(10,50,165,25);
+		frame.add(field2);
+		JButton button1 = new JButton("test");
+		button1.setBounds(10,50,165,25);
+		frame.add(button1);
 		frame.pack();
+		frame.setSize(400, 600);
 		frame.setVisible(true);
-
-
-
-
 	}
-
-	public static void main(String[] args) throws InterruptedException{
-		
-
-		
-
-	}
-
 
 	protected static List<Component> getAllComponents(final Container c) {
 		MonkeyBase test1 = new MonkeyBase("test1", 1000);
 		boolean testmode = Testing.testmode;
 		Component[] comps = c.getComponents();
 		List<Component> compList = new ArrayList<Component>();
+		for (int i = 0; i < test1.getIterations(); i++) {
+
 		for (Component comp : comps) {
 			compList.add(comp);
 			if (comp instanceof Container)
 				compList.addAll(getAllComponents((Container) comp));
-
+			if(comp instanceof JButton) { // Dealing with buttons
+				JButton button = (JButton) comp;
+				button.doClick();
+			}
 			if(comp instanceof JTextField){ // Dealing with textifled
 				JTextField mytext = (JTextField) comp;
-				for (int i = 0; i < test1.getIterations(); i++) {
-					mytext.setText(test1.randomString());
-					try {
-						if(testmode) {
-							Thread.sleep(50);
-						}
-					} catch (Exception e) {
+				mytext.setText(test1.randomString());
+				try {
+					if(testmode) {
+						Thread.sleep(50);
 					}
+				} catch (Exception e) {
 				}
-			}
 
+			}
+			
+		}
 		}   
 		return compList;
 	}}
